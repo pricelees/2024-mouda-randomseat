c3fba976fd3460c0298ce2c5a938c0d9e9532a62
pricelees <priceelees@gmail.com>
2024-07-14 21:49:31 +0900

    project setup

---
+++ src/main/java/mouda/randomseat/RandomseatApplication.java
@@ -0,0 +1,13 @@
+package mouda.randomseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mouda/randomseat/RandomseatApplicationTests.java
@@ -0,0 +1,13 @@
+package mouda.randomseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
